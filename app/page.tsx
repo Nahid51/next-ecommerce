efa12151d81cd136d6b68138c37ab8647c0de96d
Nahid Sheikh <mdnahidyn@gmail.com>
2024-07-02 09:48:59 +0600

    implemented fully responsive footer section in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,15 @@ const HomePage = () => {
     <main className=''>
       <div className="slider"><Slider /></div>
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-        <h2 className="mb-4 text-2xl">Featured Products</h2>
+        <h2 className="mb-4 text-2xl font-semibold">Featured Products</h2>
         <ProductList />
       </div>
-      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-        <h2 className="mb-4 text-2xl">Categories</h2>
+      <div className="mt-24">
+        <h2 className="mb-4 text-2xl font-semibold px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">Categories</h2>
         <CategoryList />
       </div>
-      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-        <h2 className="mb-4 text-2xl">New Products</h2>
+      <div className="my-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <h2 className="mb-4 text-2xl font-semibold">New Products</h2>
         <ProductList />
       </div>
     </main>
