9a7071e1ef9dc21e449dde5bbc6d602cfa08662b
Nahid Sheikh <mdnahidyn@gmail.com>
2024-07-10 14:18:48 +0600

    implemented email password login system

--- components/NavIcons.tsx
+++ components/NavIcons.tsx
@@ -1,59 +1,85 @@
-"use client"
+"use client";
+
+import { useWixClient } from "@/hooks/useWixClient";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import CartModal from "./CartModal";
 
 const NavIcons = () => {
     const [isProfileOpen, setIsProfileOpen] = useState(false);
     const [isCartOpen, setIsCartOpen] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
     const router = useRouter();
-    const isLoggedIn = false;
+    const pathName = usePathname();
+
+    const wixClient = useWixClient();
+    const isLoggedIn = wixClient.auth.loggedIn();
+
 
     const handleProfile = () => {
         if (!isLoggedIn) {
             router.push("/login");
+        } else {
+            setIsProfileOpen((prev) => !prev);
         }
-        setIsProfileOpen((prev) => !prev)
     };
+
+    const handleLogout = async () => {
+        setIsLoading(true);
+        Cookies.remove("refreshToken");
+        const { logoutUrl } = await wixClient.auth.logout(window.location.href);
+        setIsLoading(false);
+        setIsProfileOpen(false);
+        router.push(logoutUrl);
+    };
+
+
+    // const { cart, counter, getCart } = useCartStore();
+
+    // useEffect(() => {
+    //     getCart(wixClient);
+    // }, [wixClient, getCart]);
 
     return (
         <div className="flex items-center gap-4 xl:gap-6 relative">
             <Image
                 src="/profile.png"
-                alt="profile"
+                alt=""
                 width={22}
                 height={22}
-                className=" cursor-pointer"
+                className="cursor-pointer"
                 onClick={handleProfile}
             />
-            {
-                isProfileOpen &&
-                <div className=" absolute p-4 rounded-md top-12 left-0 text-sm shadow-xl z-20">
-                    <Link href="/">Profile</Link>
-                    <div className="mt-2 cursor-pointer">Logout</div>
+            {isProfileOpen && (
+                <div className="absolute p-4 rounded-md top-12 left-0 bg-white text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
+                    <Link href="/profile">Profile</Link>
+                    <div className="mt-2 cursor-pointer" onClick={handleLogout}>
+                        {isLoading ? "Logging out" : "Logout"}
+                    </div>
                 </div>
-            }
+            )}
             <Image
                 src="/notification.png"
-                alt="profile"
+                alt=""
                 width={22}
                 height={22}
-                className=" cursor-pointer"
+                className="cursor-pointer"
             />
-            <div className="relative cursor-pointer" onClick={() => setIsCartOpen((prev) => !prev)}>
-                <Image
-                    src="/cart.png"
-                    alt="profile"
-                    width={22}
-                    height={22}
-                />
-                <div className="absolute -top-4 -right-4 w-6 h-6 bg-ecom rounded-full text-white text-sm flex items-center justify-center">2</div>
+            {/* <div
+                className="relative cursor-pointer"
+                onClick={() => setIsCartOpen((prev) => !prev)}
+            >
+                <Image src="/cart.png" alt="" width={22} height={22} />
+                <div className="absolute -top-4 -right-4 w-6 h-6 bg-lama rounded-full text-white text-sm flex items-center justify-center">
+                    {counter}
+                </div>
             </div>
-            {isCartOpen && <CartModal />}
+            {isCartOpen && <CartModal />} */}
         </div>
-    )
-}
+    );
+};
 
 export default NavIcons;
